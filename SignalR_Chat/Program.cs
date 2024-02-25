ccdaa809fb0e0bc5a0ea9e1f32b636da29f831d5
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2024-02-25 13:32:15 +0200

    #Task5 Создание и настройка стиля главной страницы. Настройка файла  Program.cs

--- SignalR_Chat/Program.cs
+++ SignalR_Chat/Program.cs
@@ -1,27 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using SignalR_Chat;
+using SignalR_Chat.Repository;
+using SignalR_Chat.Models;// пространство имен класса ChatHub
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+var builder = WebApplication.CreateBuilder(args);
+// Для использования функциональности библиотеки SignalR,
+// в приложении необходимо зарегистрировать соответствующие сервисы
+builder.Services.AddSignalR();
+// Получаем строку подключения из файла конфигурации
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ChatContext>(options => options.UseSqlServer(connection));
+builder.Services.AddScoped<IChatRepository, ChatRepository>();
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapHub<Chat_Hub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
 
 app.Run();
